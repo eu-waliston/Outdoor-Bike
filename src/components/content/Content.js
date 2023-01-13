fb67e5eea940005b6ef8ec7dd44294a79986f821
walwizard <goneisgone@hotmail.com>
2023-01-12 23:30:26 -0300

    :) last commit

--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -5,18 +5,22 @@ const Content = () => {
   return (
     <div className="blue__circle">
       <h1 className="content__h1">
-        outdor <span>sport</span>
+        outdoor <br /> <span>sport</span>
       </h1>
       <p className="content__p">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate ut
-        ex, quam laboriosam veritatis modi nihil possimus omnis ducimus rerum,
-        placeat, consequuntur quibusdam maiores natus neque asperiores sint
-        magnam optio!
+        Lorem ipsum dowlor sit aemet connsectetur adipisesity
+      </p>
+      <p className="content__p">
+        Porsro officidis qugia, dolorifbus, nefmo error providfen
+      </p>
+      <p className="content__p">
+        Consequatur necessitatibus tempora tgotam laudantiuu
+      </p>
+      <p className="content__p">
+        Samus tempore incidunt itaque repellendus earum beat
       </p>
 
-      <button className="content__btn">
-          join now
-      </button>
+      <button className="content__btn">join now</button>
     </div>
   );
 };
